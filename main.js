e88ac2f3602bda490704c1c4cf4f12c1c7fc2d2b
MarceloCChaves <62251064+MarceloCChaves@users.noreply.github.com>
2021-01-23 14:15:06 -0300

    Add the script for the dark button

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const btn = document.querySelector('.button');
 const btnCancel = document.querySelector('.cancel');
 const Modal  = document.querySelector('.modal-overlay');
+const darkButton = document.querySelector('.dark-mode');
+const header = document.querySelector('header');
 
 btn.addEventListener('click',function(){
     Modal.classList.add('active');
 })
 btnCancel.addEventListener('click', function(){
     Modal.classList.remove('active');
+})
+darkButton.addEventListener('click',function(){
+    const element = document.body;
+    element.classList.toggle('dark-background');
+    header.classList.toggle('dark-background');
 })
